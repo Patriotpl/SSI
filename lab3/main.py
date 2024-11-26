a68ed42e58faef21fa687ba6b8f5ff774dce8881
Patriotpl <patriotpl22@gmail.com>
2024-11-26 13:28:55 +0100

    lab3

--- lab3/main.py
+++ lab3/main.py
@@ -21,8 +21,20 @@ def lab3(window: sg.Window) -> None:
         output_col = sg.Column([[]], key="-OUTPUT_COL-")
         zad1_frame = sg.Frame("Zad 1", [[input_col, output_col]], key="-ZAD1-")
         n_text = sg.Text("Podaj liczbe iteracji:")
-        n_input = sg.Spin([i for i in range(10000)], size=(10, 1))
-        zad2_frame = sg.Frame("Zad 2", [[n_text, n_input]], key="-ZAD2-")
+        n_input = sg.Spin([i for i in range(10000)], size=(10, 1), key="-N-")
+        path_desc = sg.Text("Plik spiralka")
+        zad2_path = sg.Input("", key="-ZAD2_PATH-")
+        zad2_file_button = sg.FileBrowse("Wybierz", key="-ZAD2_FILE-")
+        names_path_desc = sg.Text("plik spiralka-names")
+        zad2_names_path = sg.Input("", key="-ZAD2_NAMES_PATH-")
+        zad2_names_button = sg.FileBrowse("Wybierz", key="ZAD2_NAMES_FILE")
+        zad2_execute_button = sg.Button("Wykonaj", key="-ZAD2_EXECUTE-")
+        zad2_frame = sg.Frame("Zad 2", [
+            [n_text, n_input], 
+            [path_desc, zad2_path, zad2_file_button],
+            [names_path_desc, zad2_names_path, zad2_names_button],
+            [zad2_execute_button]
+        ], key="-ZAD2-")
         window.extend_layout(window, [[zad1_frame], [zad2_frame]])
     else:
         window["-ZAD1-"].update(visible=True)
@@ -44,7 +56,7 @@ def lab3(window: sg.Window) -> None:
             
             try:
                 with open(values["-INPUT_PATH3-"], "r") as f:
-                    data = json.load(f)
+                    zad2_data = json.load(f)
             except FileNotFoundError:
                 text = sg.Text("Plik wejscia nie istnieje.")
                 ok_button = sg.Button("Ok")
@@ -54,7 +66,7 @@ def lab3(window: sg.Window) -> None:
                 window["-INPUT_PATH3-"].update("")
 
             try:
-                table_data = probki_str_na_liczby(data["próbki_str"], [int(e) for e in data["numery_atr"]])
+                table_data = probki_str_na_liczby(zad2_data["próbki_str"], [int(e) for e in zad2_data["numery_atr"]])
             except ValueError:
                 text = sg.Text("Numery atrybutow zaiweraja znaki nienumeryczne.")
                 ok_button = sg.Button("Ok")
@@ -68,14 +80,93 @@ def lab3(window: sg.Window) -> None:
             
             table = sg.Table(       # TODO: try to make the table scrollable horizontally
                 table_data, 
-                headings=data["numery_atr"], 
+                headings=zad2_data["numery_atr"], 
                 key="-NUM_TABLE-", 
-                col_widths=[7]*len(data["numery_atr"]), 
+                col_widths=[7]*len(zad2_data["numery_atr"]), 
                 num_rows=10 if len(table_data) > 9 else len(table_data),
                 auto_size_columns=False,
                 vertical_scroll_only=False
             )
             window.extend_layout(output_col, [[table]])
+        
+        elif event == "-ZAD2_EXECUTE-":
+            if len(values["-ZAD2_PATH-"]) < 1 or len(values["-ZAD2_NAMES_PATH-"]) < 1 or int(values["-N-"]) < 1:
+                continue
+
+            zad2_data = []
+            zad2_names = []
+            try:
+                with open(values["-ZAD2_PATH-"], "r") as f:
+                    lines = f.readlines()
+                for line in lines:
+                    zad2_data.append(line.split())
+            except FileNotFoundError:
+                text = sg.Text("Nie znaleziono podanego pliku")
+                ok_button = sg.Button("-OK-")
+                new_window = sg.Window("Nie znaleziono pliku", [[text], [ok_button]])
+                event = ""
+                while event not in [sg.WIN_CLOSED, "-OK-"]:
+                    try:
+                        new_window.close()
+                    except:
+                        ...
+                    window["-ZAD2_PATH-"].update("")
+                    window["-ZAD2_NAMES_PATH-"].update("")
+                continue
+
+            try:
+                with open(values["-ZAD2_NAMES_PATH-"], "r") as f:
+                    lines = f.readlines()
+                for line in lines:
+                    zad2_names.append(line.split())
+            except FileNotFoundError:
+                text = sg.Text("Nie znaleziono podanego pliku")
+                ok_button = sg.Button("-OK-")
+                new_window = sg.Window("Nie znaleziono pliku", [[text], [ok_button]])
+                event = ""
+                while event not in [sg.WIN_CLOSED, "-OK-"]:
+                    try:
+                        new_window.close()
+                    except:
+                        ...
+                    window["-ZAD2_PATH-"].update("")
+                    window["-ZAD2_NAMES_PATH-"].update("")
+                continue
+                      
+            zad2_result = k_means(zad2_data, zad2_names, int(values["-N-"]))
+            # zad2_result_table = sg.Table(zad2_result[0], headings=[], key="-ZAD2_RESULT-")
+            # zad2_names_table = sg.Table(zad2_result[1], headings=[], key="-ZAD2_NAMES-")
+            zad2_graph = sg.Graph((200, 200), (-2, -2), (3, 3), "white")
+            exit_button = sg.Button("Wyjście", key="-ZAD2_EXIT-")
+
+            event = ""
+            new_window = sg.Window("Wynik zadania 3.2", [
+                # [zad2_result_table], 
+                # [zad2_names_table], 
+                [zad2_graph],
+                [exit_button]
+            ])
+            colors = ["red", "green", "blue", "orange"]
+            color_iter = 0
+            new_window.finalize()
+
+            ## draw coordinates
+            for i in range(-2, 3):
+                zad2_graph.draw_line((i, -2), (i, 3), "lightgray")
+                zad2_graph.draw_line((-2, i), (3, i), "lightgray")
+
+            for group in zad2_result.keys():
+                color = colors[color_iter]
+                for x, y in zad2_result[group]:
+                    zad2_graph.draw_point((x, y), 0.2, color)
+                color_iter += 1
+            while event not in [sg.WIN_CLOSED, "-ZAD2_EXIT-"]:
+                event, values = new_window.read()
+            try:
+                new_window.close()
+            except:
+                ...
+
         elif event == "home":
             window["-ZAD1-"].update(visible=False)
             window["-ZAD2-"].update(visible=False)
--- lab3/zad2.py
+++ lab3/zad2.py
@@ -1,2 +1,41 @@
-def k_means(data: list[list[float]], names: list[list[str]]) -> list[tuple[list, str]]:
-    ...
+from math import dist
+
+from random import choices
+from statistics import mean
+
+def k_means(data: list[list[float]], names: list[list[str]], n: int) -> dict:
+    m = 4
+    for i in range(len(data)):
+        data[i] = [float(data[i][0]), float(data[i][1])]
+    centers = choices(data, k=m)
+    for _ in range(n):
+        print(centers)
+        groups = {}
+        for i in range(len(centers)):
+            groups[i] = []
+        for i in range(len(data)):
+            distances = [dist(data[i], x) for x in centers]
+            u = min(distances)
+            groups[distances.index(u)].append(data[i])
+        centers = []
+        for j in groups.keys():
+            new_center = []
+            new_center.append(mean([x[0] for x in groups[j]]))
+            new_center.append(mean([x[1] for x in groups[j]]))
+            centers.append(new_center)
+        
+        return groups
+
+if __name__ == "__main__":
+    with open("data/spiralka.txt", "r") as f:
+        lines = f.readlines()
+    data = []
+    for line in lines:
+        data.append(line.split())
+    with open("data/spiralka-names.txt", "r") as f:
+        lines = f.readlines()
+    names = []
+    for line in lines:
+        names.append(line.split())
+
+    k_means(data, names, 100)
